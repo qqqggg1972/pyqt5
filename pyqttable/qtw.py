75eabf54e8f1c3d22333761cea8b973dd058543b
qianba <qqqggg1972@163.com>
2016-08-23 04:48:50 +0800

    OK

--- pyqttable/qtw.py
+++ pyqttable/qtw.py
@@ -8,8 +8,8 @@ class MyDialog(QtWidgets.QWidget):
         super(MyDialog, self).__init__(parent)
         self.setGeometry(300, 300, 450, 210)
 
-        self.MyTable = QtWidgets.QTableWidget(4,3)
-        self.MyTable.setHorizontalHeaderLabels(['姓名','身高','体重'])
+        self.MyTable = QtWidgets.QTableWidget(4, 3)
+        self.MyTable.setHorizontalHeaderLabels(['姓名', '身高', '体重'])
 
         # 防止修改
         self.MyTable.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
@@ -24,28 +24,36 @@ class MyDialog(QtWidgets.QWidget):
             headItem = self.MyTable.horizontalHeaderItem(x)  # 获得水平方向表头的Item对象
             # headItem.setTextColor(QtGui.QColor(200,111,100))
 
-
-
-
         # 单元格里加入控件
-        self.MyCombo=QtWidgets.QComboBox()
-        self.MyCombo.addItem("√")
-        self.MyCombo.addItem("×")
-        self.MyTable.setCellWidget(1,0,self.MyCombo)
+        # self.MyCombo=QtWidgets.QComboBox()
+        # self.MyCombo.addItem("√")
+        # self.MyCombo.addItem("×")
+        # self.MyTable.setCellWidget(1,0,self.MyCombo)
+
+        newItem = QtWidgets.QTableWidgetItem('松鼠松鼠松鼠松鼠松鼠')
+        self.MyTable.setItem(0, 0, newItem)
+        # newItem =
+        self.MyTable.setItem(1, 0, QtWidgets.QTableWidgetItem('大松鼠松鼠松鼠松鼠松鼠'))
+
+        self.MyTable.setItem(0, 1, QtWidgets.QTableWidgetItem('10cm'))
+        self.MyTable.setItem(0, 2, QtWidgets.QTableWidgetItem('60g'))
+
+        # 根据内容自适应列宽
+        # self.MyTable.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.ResizeToContents)
+        self.MyTable.setSortingEnabled(True)        # 排序
+        # self.MyTable.setShowGrid(False)           # 显示 格子
+        self.MyTable.setAlternatingRowColors(True)  # 隔行变色显示
+        self.MyTable.setColumnWidth(0,150)
 
 
-        newItem = QtWidgets.QTableWidgetItem('松鼠')
-        self.MyTable.setItem(0,0,newItem)
-
-        self.MyTable.setItem(0,1,QtWidgets.QTableWidgetItem('10cm'))
-        self.MyTable.setItem(0,2,QtWidgets.QTableWidgetItem('60g'))
-
-        layout=QtWidgets.QHBoxLayout()
+        layout = QtWidgets.QHBoxLayout()
         layout.addWidget(self.MyTable)
         self.setLayout(layout)
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     import sys
+
     app = QtWidgets.QApplication(sys.argv)
     myWindow = MyDialog()
     myWindow.show()
